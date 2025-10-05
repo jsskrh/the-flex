96c02b75fa7d38195035ca15d7c8f499833f7fb9
Jesse Akorah <jesseakorah@gmail.com>
2025-10-05 11:35:31 +0100

    Install packages and update config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "theflex.global" },
+      {
+        protocol: "https",
+        hostname: "hostaway-platform.s3.us-west-2.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
